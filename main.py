b5c8473a4d5e6c73ab9f960a60fd66fdff4b15f0
Mikołaj Warda <mikolaj.warda@gmail.com>
2026-02-23 17:56:33 +0100

    fix: Cleaned code by distribution to outer files

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import os
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from sqlmodel import SQLModel, Session, create_engine, Field, select, Relationship
-from dotenv import load_dotenv
-from typing import List, Optional
+from sqlmodel import SQLModel, Session, select
+from database import engine
+from models import User, FoodEntry, FoodEntryCreate
 
-load_dotenv()
-
-DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-engine = create_engine(DATABASE_URL)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,29 +12,6 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-class User(SQLModel, table=True):
-    id: int = Field(default=None, primary_key=True)
-    email: str = Field(unique=True, index=True)
-    food_entries: List["FoodEntry"] = Relationship(back_populates="user")
-
-class FoodEntry(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    food_name: str
-    description: Optional[str] = None
-    calories: int
-    protein: float
-    fat: float
-    carbohydrates: float
-    user_id: int = Field(foreign_key="user.id")
-    user: User = Relationship(back_populates="food_entries")
-    
-class FoodEntryCreate(SQLModel):
-    food_name: str
-    description: Optional[str] = None
-    calories: int
-    protein: float
-    fat: float
-    carbohydrates: float
 
 @app.post("/add-food")
 def add_food(email:str, food_data: FoodEntryCreate):
